e55cb69bd4a731f352841ffd2dde8b383c367b7a
Loboss2206 <b.logan006@gmail.com>
2023-11-06 10:53:19 +0100

    TD5 terminé

---
+++ TD5/src/echo.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    int reverse = 0;
+    if (strcmp(argv[1], "-r") == 0) {
+        reverse = 1;
+    }
+
+    if (reverse) {
+        for (int i = 2; i < argc; i++) {
+            for (int j = strlen(argv[i]); j>=0; j--) {
+                printf("%c", argv[i][j]);
+            }
+            printf(" ");
+        }
+    } else {
+        for (int i = 1; i < argc; i++) {
+            printf("%s ", argv[i]);
+        }
+    }
+
+    return 0;
+}
---
+++ TD5/src/main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *strcpy(char *dest, const char *src) {
+    char *dest_orig = dest;
+    while ((*dest++ = *src++));
+    *dest++ = '\0';
+    return dest_orig;
+}
+
+
+size_t strlen(const char *s) {
+    size_t size = 0;
+    while (*s++) size++;
+    return size;
+}
+
+char *strdup(const char *s) {
+    if (s != NULL) {
+        char *s2 = malloc(strlen(s) + 1);
+        strcpy(s2, s);
+        return s2;
+    }
+    return NULL;
+}
+
+char *strchr(const char *s, int c) {
+    while (*++s != c);
+    return (char *)s;
+}
+
+int main() {
+    const char *source = "Hello World";
+    char destination[50] = "Destination base";
+    char *return_string;
+
+    printf("destination before = \"%s\"\n", destination);
+    return_string = strcpy(destination, source);
+    printf("After strcpy, destination becomes \"%s\"\n", return_string);
+
+    const char *s = "Hello World";
+    printf("%lu\n", strlen(s));
+
+    char* new = strdup(s);
+    printf("%s\n", new);
+
+    if (new != NULL) {
+        free(new);
+    }
+
+    printf("%s", strchr(s, 'e'));
+
+    return 0;
+}
